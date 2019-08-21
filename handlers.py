409b3fea4e103fc1463ba58e3eaf88c5bc2849ce
Evgenii <abellindsey@nwe.group>
2019-08-21 22:18:44 +0300

    final but not

--- handlers.py
+++ handlers.py
@@ -1,5 +1,8 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from cv_bot import mytoken
 
+phone = mytoken.myphone
+email = mytoken.mymail
 
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id,
@@ -7,8 +10,7 @@ def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id,
                              text='Вы попали на моего CV-бота.')
     context.bot.send_message(chat_id=update.message.chat_id,
-                             text='Здесь я попытаюсь вкратце рассказать о себе, своих навыках и '
-                                                                  'об опыте работы.')
+                             text='Здесь я попытаюсь вкратце рассказать о себе и своих навыках')
 
     keyboard_for_start_bio = [[InlineKeyboardButton("Поехали!", callback_data='start bio')]]
     reply_markup = InlineKeyboardMarkup(keyboard_for_start_bio)
@@ -116,3 +118,38 @@ def about_aws(update, context):
                                  [InlineKeyboardButton("Все прочитано! Давай дальше.", callback_data='go_next')]]
     reply_markup = InlineKeyboardMarkup(keyboard_to_calm_down)
     query.message.reply_text('Расскажи мне про...', reply_markup=reply_markup)
+
+
+def go_next(update, context):
+    query = update.callback_query
+    query.edit_message_text(text="Знаю английский. Себя оцениваю на уровень"
+                                 " где-то между В1 и В2. Ближе к В2. ".format(query.data))
+    query.bot.send_message(chat_id=query.message.chat_id,
+                           text='Усидчивый, готов обучаться и впитывать как губка все новое. '
+                                'Проблем с общением в коллективе не испытываю. '
+                                'В перспективе, '
+                                'хотел бы погрузиться в ML или Big Data. Мне эти темы безумно интересны.')
+    query.bot.send_message(chat_id=query.message.chat_id,
+                           text='Опыта работы как такового было мало, так как учеба(. ')
+    query.bot.send_message(chat_id=query.message.chat_id,
+                           text='На этом, в прицнипе, всё. Мои контакты будут ниже. \n'
+                             'Мейл: {0} \nМобильник: {1} \n'.format(email, phone))
+
+    keyboard_for_bite = [[InlineKeyboardButton("Тык!", url='https://t.me/abellindsey')]]
+    reply_markup = InlineKeyboardMarkup(keyboard_for_bite)
+    query.message.reply_text('Вы можете написать мне прямо в tg: ',reply_markup=reply_markup)
+
+    keyboard_for_brief_bio = [[InlineKeyboardButton("Еще один тык", callback_data='brief_bio')]]
+    reply_markup = InlineKeyboardMarkup(keyboard_for_brief_bio)
+    query.message.reply_text('Или же посмотреть всё, полезное вам,'
+                             ' но в укороченном варианте. ', reply_markup=reply_markup)
+
+def brief_bio(update, context):
+    query = update.callback_query
+    query.edit_message_text(text="Еще раз. Только кратко.".format(query.data))
+    query.bot.send_message(chat_id=query.message.chat_id,
+                           text='Python3, django, git, mysql, типы данных, декораторы(тут они применяются '
+                                'или скоро будут), '
+                                'итераторы. Знаком с ООП. '
+                                'Английский ближе к В2, усидчивый, быстро обучаюсь и хочу учиться. '
+                                'Положил глаз на Big data и ML')
--- main.py
+++ main.py
@@ -11,12 +11,11 @@ dispatcher = updater.dispatcher
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# the very start
+
 start_handler = CommandHandler('start', handlers.start)
 dispatcher.add_handler(start_handler)
-#1st button
+
 updater.dispatcher.add_handler(CallbackQueryHandler(handlers.button_for_start_bio, pattern='start bio'))
-#2nd button
 
 updater.dispatcher.add_handler(CallbackQueryHandler(handlers.button_to_calm_down, pattern='^calm_down$'))
 
@@ -27,5 +26,9 @@ updater.dispatcher.add_handler(CallbackQueryHandler(handlers.about_IB, pattern='
 updater.dispatcher.add_handler(CallbackQueryHandler(handlers.about_back, pattern='^back$'))
 
 updater.dispatcher.add_handler(CallbackQueryHandler(handlers.about_aws, pattern='^AWS$'))
+
+updater.dispatcher.add_handler(CallbackQueryHandler(handlers.go_next, pattern='^go_next$'))
+
+updater.dispatcher.add_handler(CallbackQueryHandler(handlers.brief_bio, pattern='^brief_bio$'))
 
 updater.start_polling()
